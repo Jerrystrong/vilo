8c40d0a1e87742c5616d033b393592ac08081578
Jeremie Kimpioka <jeremiekimpioka52@gmail.com>
2026-09-20 12:35:35 +0100

    setting up nativeWind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,8 @@
 import "../global.css";
 
 import { AnimatedSplashOverlay } from "@/components/animated-icon";
-import AppTabs from "@/components/app-tabs";
 import { useFonts } from "expo-font";
-import { DarkTheme, DefaultTheme, ThemeProvider } from "expo-router";
+import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import * as splashScreen from "expo-splash-screen";
 import { useEffect } from "react";
@@ -29,7 +28,11 @@ export default function TabLayout() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <AnimatedSplashOverlay />
-      <AppTabs />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
     </ThemeProvider>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary_color:"#007B7B",
+        enabled_state: "A8C0C0",
+        whiteBg: "F4F7F6",
+        blackBg:"121818",
+        light_color:"F5F8F7",
+        hotLight:"F8F6F3",
+        seconday:"CC4500",
+      }
+    },
   },
   plugins: [],
 };
